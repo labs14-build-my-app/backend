f224eb405c3cfc1434e9d4723b3e441b717e064f
Alfonso <alfonso.garcia2324@gmail.com>
2019-08-03 07:58:42 -0700

    Added a user model

---
+++ src/models/user.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const Project = require("../models/project");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 7,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error(`Emai is invalid.`);
+        }
+      }
+    },
+    age: {
+      type: Number,
+      default: 0,
+      validate(value) {
+        if (value < 0) {
+          throw new Error("Age must be a positive number.");
+        }
+      }
+    },
+    isDeveloper: {
+      type: Boolean,
+      required: true
+    },
+    tokens: [
+      {
+        token: {
+          type: String,
+          required: true
+        }
+      }
+    ]
+  },
+  {
+    timestamps: true
+  }
+);
