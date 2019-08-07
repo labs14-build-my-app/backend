94e3ceda86b0912be926461f0e859a0bfd6d1f0f
Alfonso <alfonso.garcia2324@gmail.com>
2019-08-07 08:57:26 -0700

    Added devRouter to index.js

--- src/index.js
+++ src/index.js
@@ -6,9 +6,11 @@ const port = process.env.PORT || 8080;
 
 const projectRouter = require("./routers/project");
 const usersRouter = require("./routers/user");
+const devRouter = require("./routers/developers");
 
 app.use(express.json());
 app.use(projectRouter);
 app.use(usersRouter);
+app.use(devRouter);
 
 app.listen(port, () => console.log(`Server is up on port ${port}`));
--- src/routers/developers.js
+++ src/routers/developers.js
@@ -4,7 +4,7 @@ const User = require("../models/user");
 
 router.get("/developers", async (req, res) => {
   try {
-      const developers = await User.find({ isDeveloper: true });
+    const developers = await User.find({ isDeveloper: true });
 
     if (!developers) {
       return res.status(400).json({ error: "Unable to fetch developers." });
@@ -15,16 +15,17 @@ router.get("/developers", async (req, res) => {
   }
 });
 
-router.get('/developers/:id', async (req, res) => {
-    const _id = req.params.id;
-    try {
-      const developer = await User.findOne({ isDeveloper: true, _id });
-      if (!developer) {
-        return res.status(404).send()
-      }
-        res.status(200).send(developer);
-    } catch (error) {
-        res.status(500).send(error);
+router.get("/developers/:id", async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const developer = await User.findOne({ isDeveloper: true, _id });
+    if (!developer) {
+      return res.status(404).send();
     }
+    res.status(200).send(developer);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
+module.exports = router;
